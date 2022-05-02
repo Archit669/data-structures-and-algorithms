31cf387c3564ae61fc2eb4679aff5cfd0d910f06
Archit669 <architjain669@gmail.com>
2022-05-02 20:17:23 +0530

    assignment 2 qn4

---
+++ love babbar dsa course/2 arrays/assignment 2/4_find_first_non_repeating_element_in_an_array.cpp
@@ -0,0 +1,56 @@
+// { Driver Code Starts
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+ 
+
+ // } Driver Code Ends
+
+
+// approach - 1
+// time complexity - O(n)
+// space complexity - O(1)
+class Solution{
+    public:
+    int firstNonRepeating(int arr[], int n) 
+    { 
+        unordered_map<int,int> m;
+        
+        for (int i = 0 ; i < n ; i++){
+            m[arr[i]]++;
+        }
+        
+        for (int i = 0 ; i < n ; i++){
+            if (m[arr[i]]==1){
+                return arr[i];
+            }
+        }
+        
+        return 0;
+        
+    } 
+  
+};
+
+
+// { Driver Code Starts.
+
+int main()
+{
+
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        cin>>n;
+        int arr[n];
+        for (int i=0; i<n; i++)
+        {
+            cin>>arr[i];
+        }
+        Solution ob;
+        cout<<ob.firstNonRepeating(arr,n)<<endl;
+    }
+}
+  // } Driver Code Ends
