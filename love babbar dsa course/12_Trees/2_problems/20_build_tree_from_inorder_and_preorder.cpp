82a2daaa88220a8ef674421f2ab76762b87af738
archit669 <architjain669@gmail.com>
2022-12-18 15:03:02 +0530

    add tree questions

---
+++ love babbar dsa course/12_Trees/2_problems/20_build_tree_from_inorder_and_preorder.cpp
@@ -0,0 +1,87 @@
+//{ Driver Code Starts
+//
+
+#include<bits/stdc++.h>
+using namespace std;
+
+struct Node
+{
+	int data;
+	struct Node *left;
+	struct Node *right;
+	
+	Node(int x){
+	    data = x;
+	    left = NULL;
+	    right = NULL;
+	}
+};
+
+
+void printPostOrder(Node *root)
+{
+	if(root==NULL)
+		return;
+	printPostOrder(root->left);
+	printPostOrder(root->right);
+	cout<<root->data<<" ";
+}
+
+// } Driver Code Ends
+
+
+/*Complete the code here.
+Node is as follows:
+struct Node
+{
+  int data;
+  Node* left;
+  Node* right;
+};
+*/
+class Solution{
+    public:
+    Node* buildTree(int in[],int pre[], int n)
+    {
+        if (n <= 0) return NULL;
+        
+        Node* root = new Node(pre[0]);
+        
+        int i = 0;
+        while (i < n){
+            if (in[i] == pre[0]){
+                break;
+            }
+            i++;
+        }
+        
+        root->left = buildTree(in ,pre+1 , i);
+        root->right = buildTree(in+i+1, pre+i+1, n-i-1);
+        
+        return root;
+    }
+};
+
+//{ Driver Code Starts.
+int main()
+{
+	int t;
+	cin>>t;
+	while(t--)
+	{
+		int n;
+		cin>>n;
+		
+		int inorder[n], preorder[n];
+		for(int i=0; i<n; i++)
+			cin>> inorder[i];
+		for(int i=0; i<n; i++)
+			cin>> preorder[i];
+		Solution obj;
+		Node *root = obj.buildTree(inorder, preorder, n);
+		printPostOrder(root);
+		cout<< endl;
+	}
+}
+
+// } Driver Code Ends
---
+++ love babbar dsa course/12_Trees/2_problems/21_build_tree_from_inorder_and_postorder.cpp
@@ -0,0 +1,85 @@
+//{ Driver Code Starts
+/* program to construct tree using inorder and postorder traversals */
+#include <bits/stdc++.h>
+using namespace std;
+
+/* A binary tree node has data, pointer to left child
+   and a pointer to right child */
+struct Node {
+    int data;
+    struct Node* left;
+    struct Node* right;
+
+    Node(int x) {
+        data = x;
+        left = right = NULL;
+    }
+};
+
+/* This funtcion is here just to test buildTreeUtil() */
+void preOrder(Node* node) {
+    if (node == NULL) return;
+
+    /* then print the data of node */
+    printf("%d ", node->data);
+
+    /* first recur on left child */
+    preOrder(node->left);
+
+    /* now recur on right child */
+    preOrder(node->right);
+}
+
+Node* buildTree(int in[], int post[], int n);
+
+int main() {
+    int t, n;
+    cin >> t;
+    while (t--) {
+        cin >> n;
+        int in[n], post[n];
+        for (int i = 0; i < n; i++) cin >> in[i];
+        for (int i = 0; i < n; i++) cin >> post[i];
+        Node* root = buildTree(in, post, n);
+        preOrder(root);
+        cout << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
+
+
+/* Tree node structure
+
+struct Node
+{
+    int data;
+    struct Node* left;
+    struct Node* right;
+
+    Node(int x){
+        data = x;
+        left = right = NULL;
+    }
+};*/
+
+//Function to return a tree created from postorder and inoreder traversals.
+Node *buildTree(int in[], int post[], int n) {
+    if (n <= 0) return NULL;
+        
+    Node* root = new Node(post[n-1]);
+    
+    int i = 0;
+    while (i < n){
+        if (in[i] == post[n-1]){
+            break;
+        }
+        i++;
+    }
+    
+    root->left = buildTree(in ,post , i);
+    root->right = buildTree(in+i+1, post+i, n-i-1);
+    
+    return root;
+}
